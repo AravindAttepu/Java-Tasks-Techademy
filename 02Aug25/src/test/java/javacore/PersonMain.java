1d907093be9a42f25bece712f5a8e20baf274e40
aravind <aravindattepu@gmail.com>
2025-08-05 16:09:44 +0530

    project loaded

--- 02Aug25/src/test/java/javacore/PersonMain.java
+++ 02Aug25/src/test/java/javacore/PersonMain.java
@@ -5,6 +5,8 @@ public class PersonMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Person person= new Person("Aravind", "Attepu" ,"M");
+		//Person1_7 person= new Person1_7("Aravind", "Attepu" ,"M", "1234567890");
 		Person1_8 person= new Person1_8("Aravind", "Attepu" ,Person1_8.Gender.M,"123456789");
 		System.out.println("Person Details:");
 		System.out.println("___________________\n");
